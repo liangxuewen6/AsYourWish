ca606abdc1b107f2f470d942223b48ef7e2f198c
mikeXwang <mikexwang@163.com>
2017-03-20 22:22:41 +0800

    Add Volley test code.It will cause anr.

--- app/src/main/java/com/mikex/asyourwish/activities/MainActivity.java
+++ app/src/main/java/com/mikex/asyourwish/activities/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mBtnLogin;
     private Button mBtnClear;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,20 +37,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mBtnClear = (Button) findViewById(R.id.btn_clear);
         mBtnClear.setOnClickListener(this);
+
 //    Example of a call to a native method
 //    TextView tv = (TextView) findViewById(R.id.sample_text);
 //    tv.setText(stringFromJNI());
     }
 
-
-    /**
-     * The follow is used for test the ping interface.Maybe got anr & crash..
-     * We just need response form server.
-     * @param v
-     */
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_login:
                 try {
                     RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -74,16 +70,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             case R.id.btn_clear:
                 mTvMsg.setText("");
+
         }
-    }
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+
+
+        /**
+         * A native method that is implemented by the 'native-lib' native library,
+         * which is packaged with this application.
+         */
 //    public native String stringFromJNI();
 //
 //    // Used to load the 'native-lib' library on application startup.
 //    static {
 //        System.loadLibrary("native-lib");
 //    }
+    }
 }
