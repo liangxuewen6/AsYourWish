6eaeb2e414c46cd92063d08cb358bcdba2ff97f4
mikeXwang <mikexwang@163.com>
2017-03-20 21:51:42 +0800

    Add Volley source code as Net Connection Frame (添加volley源码作为网络链接的库)

--- app/src/main/java/com/mikex/asyourwish/activities/MainActivity.java
+++ app/src/main/java/com/mikex/asyourwish/activities/MainActivity.java
@@ -17,54 +17,65 @@ import com.example.mike.asyourwish.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private boolean isBtnPressed = false;
-    private String uriTest = "";
-    private String result;
+    private String mStringTest;
+
+    private String mTestUri = "https://www.baidu.com";
 
     private TextView mTvMsg;
+    private Button mBtnLogin;
+    private Button mBtnClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.btn_login);
-        button.setOnClickListener(this);
+        mBtnLogin = (Button) findViewById(R.id.btn_login);
+        mTvMsg = (TextView) findViewById(R.id.tv_everything_goes_well);
+        mBtnLogin.setOnClickListener(this);
+
+        mBtnClear = (Button) findViewById(R.id.btn_clear);
+        mBtnClear.setOnClickListener(this);
 //    Example of a call to a native method
 //    TextView tv = (TextView) findViewById(R.id.sample_text);
 //    tv.setText(stringFromJNI());
     }
 
+
+    /**
+     * The follow is used for test the ping interface.Maybe got anr & crash..
+     * We just need response form server.
+     * @param v
+     */
     @Override
     public void onClick(View v) {
-        if (!isBtnPressed) {
-            isBtnPressed = true;
+        switch (v.getId()){
+            case R.id.btn_login:
+                try {
+                    RequestQueue requestQueue = Volley.newRequestQueue(this);
+                    StringRequest stringRequest = new StringRequest(mTestUri,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    mStringTest = response;
+                                    mTvMsg.setText(mStringTest);
+                                    Log.d("TAG", response);
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("TAG", error.getMessage(), error);
+                        }
+                    });
+                    requestQueue.add(stringRequest);
 
-            try {
-                RequestQueue requestQueue = Volley.newRequestQueue(this);
-                StringRequest stringRequest = new StringRequest("https://www.baidu.com",
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Log.d("TAG", response);
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("TAG", error.getMessage(), error);
-                    }
-                });
-                requestQueue.add(stringRequest);
-            } catch (Exception exception) {
-
-                Thread.dumpStack();
-            } finally {
-
-            }
+                } catch (Exception exception) {
+                    Thread.dumpStack();
+                }
+            case R.id.btn_clear:
+                mTvMsg.setText("");
         }
     }
-
-
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
